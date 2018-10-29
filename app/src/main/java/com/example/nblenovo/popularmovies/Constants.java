a638d2a8b53f1a1f5ee39239c3361731ef31e4af
Viola Tarek <veolazaher@gmail.com>
2018-10-28 19:27:45 -0700

    Update Constants.java

--- app/src/main/java/com/example/nblenovo/popularmovies/Constants.java
+++ app/src/main/java/com/example/nblenovo/popularmovies/Constants.java
@@ -1,11 +1,11 @@
 package com.example.nblenovo.popularmovies;
 
 public class Constants {
-    static public String API_FULL_URL = "http://api.themoviedb.org/3/movie/popular?api_key=c2922d6e6c83600cad7a872acf059b18";
+    static public String API_FULL_URL = "http://api.themoviedb.org/3/movie/popular?api_key=your-API-keyhere";
     // this is part of the URL "PART"
     static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
     // this is the API Key
-    static public String API_KEY = "c2922d6e6c83600cad7a872acf059b18";
+    static public String API_KEY = "your-API-keyhere";
     //URL Start
     static public String IMAGE_URL = "http://image.tmdb.org/t/p/";
     //desired size
